c6019d9fc9ab49023b3b463e79a343a7ab0f52a7
yaymuffins <contact@yayponies.eu>
2021-03-03 15:00:18 +0100

    Update lcars.py

--- app/lcars.py
+++ app/lcars.py
@@ -1,6 +1,6 @@
-from screens.authorize import ScreenAuthorize
-from ui.ui import UserInterface
-import config
+from screens.authorize import ScreenAuthorize # YayMuffins - We import the function ScreenAuthorize from the module in ./screens/authorize.py
+from ui.ui import UserInterface # YayMuffins - We import the function "UserInterface" from the module in ./ui/ui.py
+import config # YayMuffins - We import the config file
 
 if __name__ == "__main__":
     firstScreen = ScreenAuthorize()
